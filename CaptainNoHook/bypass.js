055f59cfc107303b6947fd52c2a9f817d869739c
Luca Regne <45595378+luca-regne@users.noreply.github.com>
2026-07-20 10:59:11 -0300

    fix: remove commented AES hooking code

--- CaptainNoHook/bypass.js
+++ CaptainNoHook/bypass.js
@@ -10,19 +10,6 @@ Interceptor.attach(is_noncompliant_device, {
   }
 });
 
-// var cbc_init = Module.findGlobalExportByName("$s11CryptoSwift3CBCV2ivACSays5UInt8VG_tcfC");
-// Interceptor.attach(cbc_init, {
-//   onEnter(args) {
-//     console.log(`[+] CryptoSwift.CBC.init( iv: ${iv})`);
-//     // console.log(hexdump(ptr(args[0]), { length: 64 }))
-//     console.log("\n\n")
-//   },
-//   onLeave(retval) {
-//     console.log(hexdump(ptr(retval), { length: 64 }))
-//     console.log(`\t return ${retval}`);
-//   }
-// });
-
 var decrypt = Module.findGlobalExportByName("$s11CryptoSwift6CipherPAAE7decryptySays5UInt8VGAGKF");
 
 Interceptor.attach(decrypt, {
